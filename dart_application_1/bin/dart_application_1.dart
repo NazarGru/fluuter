c0c78af4d3b08ffc794fe3fcb6d68b2f4d1bda60
Ogarishev Nazar <nazarogarishev333@gmail.com>
2022-09-13 12:56:58 +0300

    type variable

--- dart_application_1/bin/dart_application_1.dart
+++ dart_application_1/bin/dart_application_1.dart
@@ -1,5 +1,23 @@
 import 'package:dart_application_1/dart_application_1.dart' as dart_application_1;
 
 void main(List<String> arguments) {
-  print('Hello world: ${dart_application_1.calculate()}!');
+  //string
+  String myString = 'Hello word';
+  //number
+  int myInt = 34;
+  //doublenumb
+  double myDouble = 34.5;
+
+  //number && double
+  
+  num myNum = 10;
+  num myNum2 = 10.6;
+
+  bool myBool = true;
+
+  dynamic myDinamic = 10;
+  print('${myString}: ${dart_application_1.calculate()}!');
+
+  print(myString.contains('Hello'));//true
 }
+
